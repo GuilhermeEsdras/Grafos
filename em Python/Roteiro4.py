ad55e32b1b9763f2fcfd320bd84a37acd301a1e8
Guilherme Esdras <guilherme.esdras@outlook.com>
2019-09-12 23:54:58 -0300

    Roteiro 4 Concluído

--- em Python/Roteiro4.py
+++ em Python/Roteiro4.py
@@ -2,24 +2,18 @@ import random
 from grafo_adj_nao_dir import Grafo
 
 # k3 = Grafo(['A', 'B', 'C'])
-# k3.adicionaAresta('A-B')
-# k3.adicionaAresta('B-C')
-# k3.adicionaAresta('C-A')
+# for aresta in ['A-B', 'B-C', 'C-A']:
 # print(k3)
 
+##
 print('Grafo da Paraíba:')
+
 paraiba = Grafo(['J', 'C', 'E', 'P', 'M', 'T', 'Z'])
-paraiba.adicionaAresta('J-C')
-paraiba.adicionaAresta('C-E')
-paraiba.adicionaAresta('C-E')
-paraiba.adicionaAresta('C-P')
-paraiba.adicionaAresta('C-P')
-paraiba.adicionaAresta('C-M')
-paraiba.adicionaAresta('C-T')
-paraiba.adicionaAresta('M-T')
-paraiba.adicionaAresta('T-Z')
-vertice_aleatorio = random.choice(paraiba.N)
+for aresta in ['J-C', 'C-E', 'C-E', 'C-P', 'C-P', 'C-M', 'C-T', 'M-T', 'T-Z']:
+    paraiba.adicionaAresta(aresta)
 print(paraiba)
+
+vertice_aleatorio = random.choice(paraiba.N)
 print('vertices_nao_adjacentes:', paraiba.vertices_nao_adjacentes())
 print('ha_laco?', paraiba.ha_laco())
 print('grau de {}:'.format(vertice_aleatorio), paraiba.grau(vertice_aleatorio))
@@ -27,15 +21,12 @@ print('ha_paralelas?', paraiba.ha_paralelas())
 print('arestas_sobre_vertice {}:'.format(vertice_aleatorio), paraiba.arestas_sobre_vertice(vertice_aleatorio))
 print('eh_completo?', paraiba.eh_completo(), '\n')
 
+##
 print('Grafo completo:')
-grafo_completo = Grafo(
-    ['J', 'C', 'E', 'P']
-)
-grafo_completo.adicionaAresta('J-C')
-grafo_completo.adicionaAresta('J-P')
-grafo_completo.adicionaAresta('J-E')
-grafo_completo.adicionaAresta('C-E')
-grafo_completo.adicionaAresta('C-P')
-grafo_completo.adicionaAresta('P-E')
+
+grafo_completo = Grafo(['J', 'C', 'E', 'P'])
+for aresta in ['J-C', 'J-P', 'J-E', 'C-E', 'C-P', 'P-E']:
+    grafo_completo.adicionaAresta(aresta)
 print(grafo_completo)
+
 print('eh_completo?', grafo_completo.eh_completo())
